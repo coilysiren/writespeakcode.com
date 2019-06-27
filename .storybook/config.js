9cf4ca6f773bcbb34acb7613f6d2ec4a54fd297b
Lynn Cyrin <lynncyrin@gmail.com>
2019-06-26 22:49:00 -0700

    checkpint

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react'
-
-function loadStories() {
-  const req = require.context('../__stories__', true, /\.stories\.js$/)
-  req.keys().forEach(filename => req(filename))
-}
-
-configure(loadStories, module)
